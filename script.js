e080144c9c8231d090194ae6d8c39aa9a7359bda
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:09:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-function threeSum(arr, target) {
-  let closestSum = Infinity;
-  let result;
-
-  arr.sort((a, b) => a - b);
-
-  for (let i = 0; i < arr.length - 2; i++) {
-    let left = i + 1;
-    let right = arr.length - 1;
-
-    while (left < right) {
-      const sum = arr[i] + arr[left] + arr[right];
-
-      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-        closestSum = sum;
-        result = closestSum;
-      }
-
-      if (sum < target) {
-        left++;
-      } else if (sum > target) {
-        right--;
-      } else {
-        return sum;
-      }
-    }
-  }
-
-  return result;
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-module.exports = threeSum;
-
+window.decimalToBinary = decimalToBinary;
